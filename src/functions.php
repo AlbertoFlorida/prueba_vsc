04cbb2efde1483e350a1e77d866ea710d48bbdeb
FloridaAlberto <alcasa07@alumnatflorida.es>
2024-09-25 20:22:05 +0200

    Modificado functions con un nuevo saludo personalizado

--- src/functions.php
+++ src/functions.php
@@ -3,6 +3,14 @@ function saludo($nombre)
 {
     return "Hola" . $nombre;
 }
-
-
 echo saludo("Manolo");
+
+
+
+
+function saludoPersonalizado($nombre)
+{
+    return "Que tal" . $nombre;
+}
+
+echo saludo("Andres");
